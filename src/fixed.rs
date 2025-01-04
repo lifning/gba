b96a9b86a959e1d97f1d6f399fb28769bfd012c4
lif <>
2025-01-04 01:25:51 -0800

    deal with these later

--- src/fixed.rs
+++ src/fixed.rs
@@ -169,6 +169,24 @@ macro_rules! impl_common_fixed_ops {
         self.0
       }
 
+      /// Makes a `Fixed` directly from a raw inner value (no shift).
+      #[inline]
+      #[must_use]
+      #[deprecated]
+      #[cfg_attr(feature = "track_caller", track_caller)]
+      pub const fn from_raw(i: $t) -> Self {
+        Self(i)
+      }
+
+      /// Unwraps the inner value directly into the base type (no shift).
+      #[inline]
+      #[must_use]
+      #[deprecated]
+      #[cfg_attr(feature = "track_caller", track_caller)]
+      pub const fn into_raw(self) -> $t {
+        self.0
+      }
+
       /// Bitwise Not.
       #[inline]
       #[must_use]
